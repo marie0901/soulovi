1bd6be01eef29e5384dc4a83b5877e53117553b0
Mariia Maliarets <mmaliarets@gmail.com>
2022-03-06 11:46:18 +0800

    UI home page

---
+++ components/ui/card-nft.js
@@ -0,0 +1,66 @@
+import Image from "next/image";
+import Link from "next/link";
+// import { AnimateKeyframes } from "react-simple-animate";
+
+export default function CardNft({ i, nft }) {
+  console.log("!!!!!nft:", nft);
+  return (
+    <div key={i} className="flex flex-wrap border-4 border-red-700">
+      <div className="grow-0 shrink-0 basis-auto w-full ">
+        <div className="flex justify-center ">
+          <div className="w-full p-4 rounded-lg shadow-lg bg-white max-w-sm">
+            <a href="#!">
+              <img
+                className="w-full object-cover h-48 shadow-lg rounded-lg mb-6"
+                src={nft.image}
+                alt={nft.name}
+              />
+            </a>
+            <div className="p-0 border-4 border-blue-700 flex">
+              <div className=" mb-2 w-3/4">
+                <div className="text-gray-900 text-xl font-medium mb-2  truncate">
+                  {nft.name}
+                </div>
+                <p className="text-gray-400 font-mono text-sm ">Axionov</p>
+              </div>
+              <div className="flex flex-col">
+                <div className="text-mono text-xs text-gray-500">Price</div>
+                <div className="text-xs flex">
+                  <span className="inline-block pt-1">
+                    <img
+                      className="h-3 inline-block"
+                      src="/images/eth-symbol.svg"
+                    ></img>
+                  </span>
+
+                  <div className="pt-1">{nft.price}</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    // <div key={i} className="border shadow rounded-xl overflow-hidden">
+    //   <img src={nft.image} />
+    //   <div className="p-4">
+    //     <p style={{ height: "64px" }} className="text-2xl font-semibold">
+    //       {nft.name}
+    //     </p>
+    //     <div style={{ height: "70px", overflow: "hidden" }}>
+    //       <p className="text-gray-400">{nft.description}</p>
+    //     </div>
+    //   </div>
+    //   <div className="p-4 bg-black">
+    //     <p className="text-2xl font-bold text-white">{nft.price} ETH</p>
+    //     <button
+    //       className="mt-4 w-full bg-pink-500 text-white font-bold py-2 px-12 rounded"
+    //       onClick={() => buyNft(nft)}
+    //     >
+    //       Buy
+    //     </button>
+    //   </div>
+    // </div>
+  );
+}
--- pages/all-nfts.js
+++ pages/all-nfts.js
@@ -4,6 +4,7 @@ import axios from "axios";
 import Web3Modal from "web3modal";
 
 import { marketplaceAddress } from "../config";
+import CardNft from "@components/ui/card-nft";
 
 import NFTMarketplace from "../utils/NFTMarketplace.json";
 if (typeof window === "undefined") {
@@ -29,6 +30,11 @@ export default function Home() {
       provider
     );
     const data = await contract.fetchMarketItems();
+
+    /*
+     *  map over items returned from smart contract and format
+     *  them as well as fetch their token metadata
+     */
 
     /*
      *  map over items returned from smart contract and format
@@ -75,39 +81,46 @@ export default function Home() {
     await transaction.wait();
     loadNFTs();
   }
+
   if (loadingState === "loaded" && !nfts.length)
-    return <h1 className="px-20 py-10 text-3xl">No items in marketplace</h1>;
+    return (
+      <div className="container">
+        <div className="flex px-4 py-4">
+          <h2 class="font-medium leading-tight text-4xl mt-0 mb-2 text-gray-800">
+            All NFTs
+          </h2>
+          <div className="text-gray-500 text-xs font-mono pl-3 pt-6 tracking-widest align-bottom">
+            {nfts.length} items listed
+          </div>
+        </div>
+
+        <h1 className="px-20 py-10 text-3xl">No items in marketplace</h1>
+      </div>
+    );
   return (
-    <div className="flex justify-center">
-      <div className="px-4" style={{ maxWidth: "1600px" }}>
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-4">
-          {nfts.map((nft, i) => (
-            <div key={i} className="border shadow rounded-xl overflow-hidden">
-              <img src={nft.image} />
-              <div className="p-4">
-                <p
-                  style={{ height: "64px" }}
-                  className="text-2xl font-semibold"
-                >
-                  {nft.name}
-                </p>
-                <div style={{ height: "70px", overflow: "hidden" }}>
-                  <p className="text-gray-400">{nft.description}</p>
-                </div>
-              </div>
-              <div className="p-4 bg-black">
-                <p className="text-2xl font-bold text-white">{nft.price} ETH</p>
-                <button
-                  className="mt-4 w-full bg-pink-500 text-white font-bold py-2 px-12 rounded"
-                  onClick={() => buyNft(nft)}
-                >
-                  Buy
-                </button>
-              </div>
-            </div>
-          ))}
+    <div className="container">
+      <div className="flex px-4 py-4">
+        <h2 class="font-medium leading-tight text-4xl mt-0 mb-2 text-gray-800">
+          All NFTs
+        </h2>
+        <div className="text-gray-500 text-xs font-mono pl-3 pt-6 tracking-widest align-bottom">
+          {nfts.length} items listed
         </div>
       </div>
+      {loadingState === "loaded" ? (
+        <div className="flex justify-center">
+          <div className="px-4" style={{ maxWidth: "1600px" }}>
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-4">
+              {nfts.map((nft, i) => (
+                // <div>jkjkjkjk</div>
+                <CardNft key={i} nft={nft}></CardNft>
+              ))}
+            </div>
+          </div>
+        </div>
+      ) : (
+        <div>LOADING...</div>
+      )}
     </div>
   );
 }
