1988b9d2fc7ab9563054464ad91f7470860f1a40
Mariia Maliarets <mmaliarets@gmail.com>
2022-05-14 20:24:23 +0200

    make UI changes according the soulovi-landing

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,10 @@
 const colors = require('tailwindcss/colors');
 
 module.exports = {
-  content: ['pages/**/*.{js,ts,jsx,tsx}', 'components/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    'src/pages/**/*.{js,ts,jsx,tsx}',
+    'src/components/**/*.{js,ts,jsx,tsx}',
+  ],
   theme: {
     extend: {},
     fontFamily: {
